2e045f07670e4c308479379864928fde6a85b5ac
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2025-05-12 03:07:45 -0300

    FEAT move the app and main to the app folder

---
+++ proyectos/mouse-follower/src/app/App.jsx
@@ -0,0 +1,10 @@
+import { MouseFollower } from "../features/mouseFollower/MouseFollower" 
+function App () {
+  return (
+    <main>
+      <MouseFollower />
+    </main>
+  )
+}
+
+export default App
