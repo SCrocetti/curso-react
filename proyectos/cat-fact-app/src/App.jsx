e432b7b7522e5f62febca3aa61a2e541f0a2ee83
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2025-05-15 18:23:48 -0300

    FEAT add a load fact button and put a spinner while the image loads

--- proyectos/cat-fact-app/src/App.jsx
+++ proyectos/cat-fact-app/src/App.jsx
@@ -7,28 +7,34 @@ export const App = () => {
     const [catFact, setCatFact] = useState(null);
     const [catImage, setCatImage] = useState(null);
     const [error, setError] = useState(null);
-
-    useEffect(() =>{
+    const [loading, setLoading] = useState(false);
+    const fetchCatFact = () => {
         fetch(CAT_FACT_API_URL)
-            .then((response)=> {
+            .then((response) => {
                 if (!response.ok) {
                     throw new Error('Error with cat fact resp');
                 }
-                return response.json()
+                return response.json();
             })
             .then((data) => {
-                const newFact= data.fact;
+                const newFact = data.fact;
                 setCatFact(newFact);
             })
             .catch((err) => {
                 console.error(err);
                 setError(`Error fetching fact: ${err.message}`);
+                setCatFact(null);
             });
+    }
+    useEffect(() =>{
+        fetchCatFact();
     },[]);
     useEffect(() => {
         if (catFact) {
-            const firstWord = catFact.split(' ')[0];
-            fetch(`${CAT_IMAGE_API_BASE_URL}/cat/says/${firstWord}?json=true&`)
+            setError(null);
+            const firstWords = catFact.split(' ').slice(0, 3).join(' ');
+            setLoading(true);
+            fetch(`${CAT_IMAGE_API_BASE_URL}/cat/says/${firstWords}?json=true`)
                 .then((response) => {
                     if (!response.ok) {
                         throw new Error('Error with cat image resp');
@@ -42,19 +48,30 @@ export const App = () => {
                 .catch((err) => {
                     console.error(err);
                     setError(`Error fetching image: ${err.message}`);
-                });
+                    setCatImage(null);
+                    setLoading(false);
+                })
         }
     }, [catFact]);
+    const handleClick = () => {
+        fetchCatFact();
+    };
+    const handleLoading = () => {  
+        setLoading(false);
+    }
+    const imageClass = !loading ? 'cat-image' : 'cat-image hidden';
     return (
         <main>
             <h1>Cat Fact App</h1>
             {error && <p className="error">{error}</p>}
+            <button onClick={handleClick}>Get new cat fact</button>
             {catFact && (
                 <section className="cat-fact">
-                    <p>{catFact}</p>
-                    {catImage && <img src={catImage} className="cat-image" alt="Cat image generated with the first word of the fact" />}
-                </section>
-            )}
+                    <p >{catFact}</p>
+                    {loading && <div className="spinner" />}
+                    {(catImage) && <img src={catImage} className={imageClass} onLoad={handleLoading} alt="Cat image generated with the first words of the fact" />}
+                </section> )}
+            
         </main>
     );
 }
